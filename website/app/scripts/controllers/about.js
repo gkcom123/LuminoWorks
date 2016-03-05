6ed3d5d40e06e090e36c14a6e48ba37d0976bb38
Gunjan Kumar <kumargun@yahoo.com>
2016-03-05 22:53:48 +0000

    First Commit

---
+++ website/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lWorksApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lWorksApp
+ */
+angular.module('lWorksApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
